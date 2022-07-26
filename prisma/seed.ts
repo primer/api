567390ba717c02a9a632bb92f7bacbd3614773b8
Cole Bemis <colebemis@github.com>
2022-07-26 10:45:12 -0700

    Update seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,16 +4,22 @@
 // various data sources using the GraphQL mutation API.
 
 import { PrismaClient } from "@prisma/client";
-import fetch from "node-fetch";
-import { pascalCase, sentenceCase } from "change-case";
+// import fetch from "node-fetch";
+// import { pascalCase, sentenceCase } from "change-case";
+// @ts-ignore
+// import octicons from "@primer/octicons";
+// import primitives from "@primer/primitives";
+// import flatten from "flat";
 
 const prisma = new PrismaClient();
 
 async function main() {
   await prisma.$connect();
 
+  /*
+
   // Fetch React component metadata from the docs
-  const reactComponents: Array<{ id: string }> = await fetch(
+  const reactComponents: Array<{ id: string; status: string }> = await fetch(
     `https://primer.github.io/react/components.json`
   )
     .then((res) => res.json())
@@ -21,11 +27,17 @@ async function main() {
       console.error(error);
     });
 
-  // Upsert component metadata into the database
+  const viewComponents: Array<{ id: string; status: string }> = await fetch(
+    `https://primer.github.io/view_components/components.json`
+  )
+    .then((res) => res.json())
+    .catch((error) => {
+      console.error(error);
+    });
 
-  const data = await Promise.all(
-    reactComponents.slice(0, 10).map(async (component) => {
-      console.log(pascalCase(component.id));
+  // Upsert component metadata into the database
+  const reactdata = await Promise.all(
+    reactComponents.map(async (component) => {
       const componentName = sentenceCase(component.id);
       return prisma.componentImpl.upsert({
         where: {
@@ -34,7 +46,11 @@ async function main() {
             framework: "REACT",
           },
         },
-        update: {},
+        update: {
+          framework: "REACT",
+          status: "ALPHA",
+          source: "https://github.com/primer/react/tree/main/src", // TODO: Update this
+        },
         create: {
           component: {
             connectOrCreate: {
@@ -48,12 +64,78 @@ async function main() {
           },
           framework: "REACT",
           status: "ALPHA",
+          source: "https://github.com/primer/react/tree/main/src", // TODO: Update this
         },
       });
     })
   );
 
-  console.log(data);
+  const railsData = await Promise.all(
+    viewComponents.map(async (component) => {
+      const componentName = sentenceCase(component.id);
+      return prisma.componentImpl.upsert({
+        where: {
+          componentName_framework: {
+            componentName,
+            framework: "RAILS",
+          },
+        },
+        update: {
+          framework: "RAILS",
+          status: "ALPHA",
+          source:
+            "https://github.com/primer/view_components/tree/main/app/components/primer", // TODO: Update this
+        },
+        create: {
+          component: {
+            connectOrCreate: {
+              where: {
+                name: componentName,
+              },
+              create: {
+                name: componentName,
+              },
+            },
+          },
+          framework: "RAILS",
+          status: "ALPHA",
+          source:
+            "https://github.com/primer/view_components/tree/main/app/components/primer", // TODO: Update this
+        },
+      });
+    })
+  );
+  */
+
+  // Octicons
+
+  // prisma.octicon.deleteMany({});
+  /*const iconData = await prisma.octicon.createMany({
+    data: Object.values(octicons).map((icon: any) => {
+      console.log(icon.name);
+      return {
+        name: icon.name,
+      };
+    }),
+  });
+  */
+
+  // const tokenData = await prisma.designToken.createMany({
+  //   data: Object.keys(flatten(primitives.colors.light, { delimiter: "-" })).map(
+  //     (key) => ({ name: key })
+  //   ),
+  // });
+
+  // await prisma.component.update({
+  //   where: {
+  //     name: "Branch name",
+  //   },
+  //   data: {
+  //     designTokensUsed: {
+  //       connect: [{ name: "accent-fg" }],
+  //     },
+  //   },
+  // });
 }
 
 main()
