231dc45ebf66edcd03cf455ba27a5cd0daa60bda
Cole Bemis <colebemis@github.com>
2022-07-26 12:34:33 -0700

    Install @primer/react

--- examples/docs/app/root.tsx
+++ examples/docs/app/root.tsx
@@ -12,6 +12,7 @@ import { json } from "@remix-run/node";
 import { gql } from "graphql-request";
 import { primerApi } from "~/primer-api-client";
 import { paramCase } from "change-case";
+import { ThemeProvider, BaseStyles, NavList, PageLayout } from "@primer/react";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -38,26 +39,40 @@ export const loader = async () => {
 
 export default function App() {
   const data = useLoaderData<typeof loader>();
+
   return (
     <html lang="en">
       <head>
         <Meta />
         <Links />
+        {typeof document === "undefined" ? "__STYLES__" : null}
       </head>
       <body>
-        <nav>
-          <ul>
-            {data.components.map(({ name }) => (
-              <li key={name}>
-                <a href={`/${paramCase(name)}`}>{name}</a>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+        <ThemeProvider>
+          <BaseStyles>
+            <PageLayout containerWidth="full">
+              <PageLayout.Pane position="start">
+                <NavList>
+                  <NavList.Group title="Components">
+                    {data.components
+                      .sort((a, b) => (a.name > b.name ? 1 : -1))
+                      .map(({ name }) => (
+                        <NavList.Item key={name} href={`/${paramCase(name)}`}>
+                          {name}
+                        </NavList.Item>
+                      ))}
+                  </NavList.Group>
+                </NavList>
+              </PageLayout.Pane>
+              <PageLayout.Content>
+                <Outlet />
+              </PageLayout.Content>
+            </PageLayout>
+          </BaseStyles>
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </ThemeProvider>
       </body>
     </html>
   );
