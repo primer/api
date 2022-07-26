b06d987acf1e7ff787b9e6da2bc9c9f61c21a280
Cole Bemis <colebemis@github.com>
2022-07-26 12:23:05 -0700

    Set up Primer API client

---
+++ examples/docs/app/primer-api-client.ts
@@ -0,0 +1,3 @@
+import { GraphQLClient } from "graphql-request";
+
+export const primerApi = new GraphQLClient("http://localhost:4000");
--- examples/docs/app/root.tsx
+++ examples/docs/app/root.tsx
@@ -6,15 +6,38 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { gql } from "graphql-request";
+import { primerApi } from "~/primer-api-client";
+import { paramCase } from "change-case";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Primer",
   viewport: "width=device-width,initial-scale=1",
 });
 
+// TODO: Generate this type from the GraphQL schema
+type LoaderData = {
+  components: Array<{ name: string }>;
+};
+
+export const loader = async () => {
+  const data = await primerApi.request<LoaderData>(gql`
+    {
+      components {
+        name
+      }
+    }
+  `);
+
+  return json(data);
+};
+
 export default function App() {
+  const data = useLoaderData<typeof loader>();
   return (
     <html lang="en">
       <head>
@@ -22,6 +45,15 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <nav>
+          <ul>
+            {data.components.map(({ name }) => (
+              <li key={name}>
+                <a href={`/${paramCase(name)}`}>{name}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
